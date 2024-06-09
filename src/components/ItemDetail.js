62f2d402e6b8f025f8eae83d1f92c274811d26cc
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-09 12:01:27 +0530

    new commit

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Routes,Route, useParams}from "react-router-dom"
+
+const ItemDetail = () => {
+   let {itemId}=useParams();
+  return (
+    <div><h1>Item {itemId}</h1>
+    <br></br>
+    <p>Description for {itemId}</p></div>
+  )
+}
+
+export default ItemDetail
---
+++ src/components/ItemList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Routes,Route,Link}from "react-router-dom"
+import ItemDetail from './ItemDetail'
+
+const ItemList = () => {
+    const items=[{Id:"1",desc:"Item1"},{Id:"2",desc:"Item2"},{Id:"3",desc:"Item3"},{Id:"4",desc:"Item4"}]
+  return (
+    <div><h1>ItemList</h1>
+    {<ul>
+        
+        
+        {
+            items.map((item)=>{
+              return  <li><Link to={`/items/${item.Id}`}>{item.desc}</Link></li>
+            })
+        }
+        </ul>}
+    
+    
+    
+    
+    </div>
+    
+  )
+}
+
+export default ItemList
